7546341f7e8128cdcc2a81fee15ec3006ad5527f
David Konsumer <konsumer@jetboystudio.com>
2023-11-10 04:35:49 -0800

    some selects

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import INI from 'ini'
 import exampleEvent from './examples/event.ito?raw'
 import exampleEnemy from './examples/enemy.ito?raw'
 import exampleCharacter from './examples/character.ito?raw'
+import lists from './lists.json'
 
 const { event } = INI.parse(exampleEvent)
 const { enemy } = INI.parse(exampleEnemy)
@@ -15,13 +16,24 @@ function toTitleCase(text) {
   );
 }
 
-console.log(Object.keys(character).map(name => `<InputText name='${name}' label='${toTitleCase(name)}' placeholder={character.${name}} />`).join('\n'))
+console.log(Object.keys(character).map(name => `<InputText name='${name}' label='${toTitleCase(name)}' defaultValue={character.${name}} />`).join('\n'))
 
 function InputText({name, label, ...props}) {
   return (
     <div className="field">
       <label htmlFor={name}>{label}</label>
       <input type="text" name={name} {...props} />
+    </div>
+  )
+}
+
+function InputSelect({name, label, options, ...props}) {
+  return (
+    <div className="field">
+      <label htmlFor={name}>{label}</label>
+      <select type="text" name={name} {...props}>
+        {options.map((o, i) => (<option key={i}>{o}</option>))}
+      </select>
     </div>
   )
 }
@@ -42,58 +54,58 @@ function App() {
       
       {tab === 'event' && (
         <form className="tab-content">
-          <InputText name='name' label='Name' placeholder={event.name} />
-          <InputText name='location' label='Location' placeholder={event.location} />
-          <InputText name='author' label='Author' placeholder={event.author} />
-          <InputText name='contact' label='Contact' placeholder={event.contact} />
-          <InputText name='flavor' label='Flavor' placeholder={event.flavor} />
-          <InputText name='options' label='Options' placeholder={event.options} />
-          <InputText name='image' label='Image' placeholder={event.image} />
-          <InputText name='about' label='About' placeholder={event.about} />
+          <InputText name='name' label='Name' defaultValue={event.name} />
+          <InputSelect name='location' label='Location' defaultValue={event.location} options={lists.locations} />
+          <InputText name='author' label='Author' defaultValue={event.author} />
+          <InputText name='contact' label='Contact' defaultValue={event.contact} />
+          <InputText name='flavor' label='Flavor' defaultValue={event.flavor} />
+          <InputText name='options' label='Options' defaultValue={event.options} />
+          <InputText name='image' label='Image' defaultValue={event.image} />
+          <InputText name='about' label='About' defaultValue={event.about} />
           
-          <InputText name='optiona' label='Option A' placeholder={event.optiona} />
-          <InputText name='testa' label='Test A' placeholder={event.testa} />
-          <InputText name='successa' label='Success A' placeholder={event.successa} />
-          <InputText name='winprizea' label='Win Prize A' placeholder={event.winprizea} />
-          <InputText name='winnumbera' label='Win Number A' placeholder={event.winnumbera} />
-          <InputText name='failurea' label='Failure A' placeholder={event.failurea} />
-          <InputText name='failprizea' label='Fail Prize A' placeholder={event.failprizea} />
-          <InputText name='failnumbera' label='Fail Number A' placeholder={event.failnumbera} />
+          <InputText name='optiona' label='Option A' defaultValue={event.optiona} />
+          <InputSelect name='testa' label='Test A' defaultValue={event.testa} options={lists.checks} />
+          <InputText name='successa' label='Success A' defaultValue={event.successa} />
+          <InputText name='winprizea' label='Win Prize A' defaultValue={event.winprizea} />
+          <InputText name='winnumbera' label='Win Number A' defaultValue={event.winnumbera} />
+          <InputText name='failurea' label='Failure A' defaultValue={event.failurea} />
+          <InputText name='failprizea' label='Fail Prize A' defaultValue={event.failprizea} />
+          <InputText name='failnumbera' label='Fail Number A' defaultValue={event.failnumbera} />
 
-          <InputText name='optionb' label='Option B' placeholder={event.optionb} />
-          <InputText name='testb' label='Test B' placeholder={event.testb} />
-          <InputText name='successb' label='Success B' placeholder={event.successb} />
-          <InputText name='winprizeb' label='Win Prize B' placeholder={event.winprizeb} />
-          <InputText name='winnumberb' label='Win Number B' placeholder={event.winnumberb} />
-          <InputText name='failureb' label='Failure B' placeholder={event.failureb} />
-          <InputText name='failprizeb' label='Fail Prize B' placeholder={event.failprizeb} />
-          <InputText name='failnumberb' label='Fail Number B' placeholder={event.failnumberb} />
+          <InputText name='optionb' label='Option B' defaultValue={event.optionb} />
+          <InputSelect name='testb' label='Test B' defaultValue={event.testb} options={lists.checks} />
+          <InputText name='successb' label='Success B' defaultValue={event.successb} />
+          <InputText name='winprizeb' label='Win Prize B' defaultValue={event.winprizeb} />
+          <InputText name='winnumberb' label='Win Number B' defaultValue={event.winnumberb} />
+          <InputText name='failureb' label='Failure B' defaultValue={event.failureb} />
+          <InputText name='failprizeb' label='Fail Prize B' defaultValue={event.failprizeb} />
+          <InputText name='failnumberb' label='Fail Number B' defaultValue={event.failnumberb} />
           
           <button>SAVE</button>
         </form>
       )}
       {tab === 'enemy' && (
         <form className="tab-content">
-          <InputText name='name' label='Name' placeholder={enemy.name} />
-          <InputText name='subtitle' label='Subtitle' placeholder={enemy.subtitle} />
-          <InputText name='type' label='Type' placeholder={enemy.type} />
-          <InputText name='location' label='Location' placeholder={enemy.location} />
-          <InputText name='author' label='Author' placeholder={enemy.author} />
-          <InputText name='intro' label='Intro' placeholder={enemy.intro} />
-          <InputText name='can_run' label='Can Run?' placeholder={enemy.can_run} />
-          <InputText name='health' label='Health' placeholder={enemy.health} />
-          <InputText name='power' label='Power' placeholder={enemy.power} />
-          <InputText name='damagevalue' label='Damage Value' placeholder={enemy.damagevalue} />
-          <InputText name='damagetype' label='Damage Type' placeholder={enemy.damagetype} />
-          <InputText name='exp' label='Exp' placeholder={enemy.exp} />
-          <InputText name='prize_type' label='Prize Type' placeholder={enemy.prize_type} />
-          <InputText name='prize_name' label='Prize Name' placeholder={enemy.prize_name} />
-          <InputText name='hit01' label='Hit 01' placeholder={enemy.hit01} />
-          <InputText name='hit02' label='Hit 02' placeholder={enemy.hit02} />
-          <InputText name='hit03' label='Hit 03' placeholder={enemy.hit03} />
-          <InputText name='art01' label='Art 01' placeholder={enemy.art01} />
-          <InputText name='art02' label='Art 02' placeholder={enemy.art02} />
-          <InputText name='artfreq' label='Art Frequency' placeholder={enemy.artfreq} />
+          <InputText name='name' label='Name' defaultValue={enemy.name} />
+          <InputText name='subtitle' label='Subtitle' defaultValue={enemy.subtitle} />
+          <InputText name='type' label='Type' defaultValue={enemy.type} />
+          <InputSelect name='location' label='Location' defaultValue={enemy.location} options={lists.locations} />
+          <InputText name='author' label='Author' defaultValue={enemy.author} />
+          <InputText name='intro' label='Intro' defaultValue={enemy.intro} />
+          <InputText name='can_run' label='Can Run?' defaultValue={enemy.can_run} />
+          <InputText name='health' label='Health' defaultValue={enemy.health} />
+          <InputText name='power' label='Power' defaultValue={enemy.power} />
+          <InputText name='damagevalue' label='Damage Value' defaultValue={enemy.damagevalue} />
+          <InputText name='damagetype' label='Damage Type' defaultValue={enemy.damagetype} />
+          <InputText name='exp' label='Exp' defaultValue={enemy.exp} />
+          <InputText name='prize_type' label='Prize Type' defaultValue={enemy.prize_type} />
+          <InputText name='prize_name' label='Prize Name' defaultValue={enemy.prize_name} />
+          <InputText name='hit01' label='Hit 01' defaultValue={enemy.hit01} />
+          <InputText name='hit02' label='Hit 02' defaultValue={enemy.hit02} />
+          <InputText name='hit03' label='Hit 03' defaultValue={enemy.hit03} />
+          <InputText name='art01' label='Art 01' defaultValue={enemy.art01} />
+          <InputText name='art02' label='Art 02' defaultValue={enemy.art02} />
+          <InputText name='artfreq' label='Art Frequency' defaultValue={enemy.artfreq} />
 
           <button>SAVE</button>
         </form>
@@ -101,26 +113,26 @@ function App() {
       
       {tab === 'character' && (
         <form className="tab-content">
-          <InputText name='name' label='Name' placeholder={character.name} />
-          <InputText name='author' label='Author' placeholder={character.author} />
-          <InputText name='contact' label='Contact' placeholder={character.contact} />
-          <InputText name='strength' label='Strength' placeholder={character.strength} />
-          <InputText name='dexterity' label='Dexterity' placeholder={character.dexterity} />
-          <InputText name='perception' label='Perception' placeholder={character.perception} />
-          <InputText name='charisma' label='Charisma' placeholder={character.charisma} />
-          <InputText name='knowledge' label='Knowledge' placeholder={character.knowledge} />
-          <InputText name='luck' label='Luck' placeholder={character.luck} />
-          <InputText name='sprite_icon' label='Sprite Icon' placeholder={character.sprite_icon} />
-          <InputText name='sprite_back' label='Sprite Back' placeholder={character.sprite_back} />
-          <InputText name='sprite_house' label='Sprite House' placeholder={character.sprite_house} />
-          <InputText name='portrait_a' label='Portrait A' placeholder={character.portrait_a} />
-          <InputText name='name_a' label='Name A' placeholder={character.name_a} />
-          <InputText name='portrait_b' label='Portrait B' placeholder={character.portrait_b} />
-          <InputText name='name_b' label='Name Back' placeholder={character.name_b} />
-          <InputText name='menu_tag' label='Menu Tag' placeholder={character.menu_tag} />
-          <InputText name='menu_desc' label='Menu Description' placeholder={character.menu_desc} />
-          <InputText name='perkpack_a' label='Perk Pack A' placeholder={character.perkpack_a} />
-          <InputText name='perkpack_b' label='Perk Pack B' placeholder={character.perkpack_b} />
+          <InputText name='name' label='Name' defaultValue={character.name} />
+          <InputText name='author' label='Author' defaultValue={character.author} />
+          <InputText name='contact' label='Contact' defaultValue={character.contact} />
+          <InputText name='strength' label='Strength' defaultValue={character.strength} />
+          <InputText name='dexterity' label='Dexterity' defaultValue={character.dexterity} />
+          <InputText name='perception' label='Perception' defaultValue={character.perception} />
+          <InputText name='charisma' label='Charisma' defaultValue={character.charisma} />
+          <InputText name='knowledge' label='Knowledge' defaultValue={character.knowledge} />
+          <InputText name='luck' label='Luck' defaultValue={character.luck} />
+          <InputText name='sprite_icon' label='Sprite Icon' defaultValue={character.sprite_icon} />
+          <InputText name='sprite_back' label='Sprite Back' defaultValue={character.sprite_back} />
+          <InputText name='sprite_house' label='Sprite House' defaultValue={character.sprite_house} />
+          <InputText name='portrait_a' label='Portrait A' defaultValue={character.portrait_a} />
+          <InputText name='name_a' label='Name A' defaultValue={character.name_a} />
+          <InputText name='portrait_b' label='Portrait B' defaultValue={character.portrait_b} />
+          <InputText name='name_b' label='Name Back' defaultValue={character.name_b} />
+          <InputText name='menu_tag' label='Menu Tag' defaultValue={character.menu_tag} />
+          <InputText name='menu_desc' label='Menu Description' defaultValue={character.menu_desc} />
+          <InputText name='perkpack_a' label='Perk Pack A' defaultValue={character.perkpack_a} />
+          <InputText name='perkpack_b' label='Perk Pack B' defaultValue={character.perkpack_b} />
           
           <button>SAVE</button>
         </form>
